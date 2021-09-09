f17ce75acbd84a82a20ca0166be8afa86d3bf7a5
MeowMeowZi <1649501854@qq.com>
2021-09-09 21:43:31 +0800

    add pause menu

---
+++ SunnyLand/Assets/Scripts/Pause.cs
@@ -0,0 +1,25 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    public AudioMixer audioMixer;
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void SetVolume(float value)
+    {
+        audioMixer.SetFloat("MainVolume", value);
+    }
+}
