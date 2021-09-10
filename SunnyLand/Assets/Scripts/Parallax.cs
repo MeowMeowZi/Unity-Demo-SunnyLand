df39e229c3b389a771a56384969f5a042920c35f
MeowMeowZi <1649501854@qq.com>
2021-09-10 21:48:19 +0800

    add parallax

---
+++ SunnyLand/Assets/Scripts/Parallax.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+    public Transform cam;
+    public float moveRate;
+    public bool lockY;
+    private float startPointX, startPointY;
+
+
+    void Start()
+    {
+        startPointX = cam.position.x;
+        startPointY = cam.position.y;
+    }
+
+    void Update()
+    {
+        if (lockY)
+        {
+            transform.position = new Vector2(startPointX + cam.position.x * moveRate, cam.position.y);
+            Debug.Log("lockY");
+        }   
+        else
+        {
+            transform.position = new Vector2(startPointX + cam.position.x * moveRate, startPointY + cam.position.y);
+        }
+    }
+}
